41ad0ae863d7c3c7a88918cacf096d6beb584166
Raenique27 <raeniquewalters@gmail.com>
2022-11-22 20:04:26 -0500

    installed gh-pages

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -102,10 +102,10 @@ const PhotoList = ({ category }) => {
     },
   ]);
 
-  const currentPhotos = photos.filter((photo) => photo.category === category);
+  const currentPhotos = photos.filter(photo => photo.category === category);
   const [currentPhoto, setCurrentPhoto] = useState();
   const toggleModal = (image, i) => {
-    setCurrentPhoto({...image, index: i})
+    setCurrentPhoto({...image, index: i});
     setIsModalOpen(!isModalOpen);
   }
   return (
@@ -114,7 +114,7 @@ const PhotoList = ({ category }) => {
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
-            src={require(`../../assets/small/${category}/${i}.jpg`)}
+            src={require(`../../assets/small/${category}/${i}.jpg`).default}
             alt={image.name}
             className="img-thumbnail mx-1"
             onClick={() => toggleModal(image,i)}
